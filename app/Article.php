220bf20322399f968b2361173e88a1b49e04b350
MyUsername <emanuele.marini37@gmail.com>
2021-10-03 11:30:58 +0200

    check

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    //c'è la foreign key assegno il "belongsTo" relativo alla tabella 
+    //collegata ovvero Author
+    public function author() {
+          return $this->belongsTo(Author::class);
+    }
 }
--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    //assegno la relazione hasMany con la tabella article
+    public function article(){
+          return $this->hasMany(Article::class);
+    }
 }
